cc34b207155a98b377c6ee04927085eae55d8305
Anubhav Dixit <anubhavdxt46@gmail.com>
2022-06-26 20:32:28 +0530

    Hide testimonial section

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -6,16 +6,17 @@ import React from "react";
 const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
-      {["home", "about", "work", "skills", "testimonials", "contact"].map(
-        (item, index) => (
+      {
+        // ["home", "about", "work", "skills", "testimonials", "contact"]
+        ["home", "about", "work", "skills", "contact"].map((item, index) => (
           <a
             href={`#${item}`}
             key={item + index}
             className="app__navigation-dot"
             style={active === item ? { backgroundColor: "#60527F" } : {}}
           />
-        )
-      )}
+        ))
+      }
     </div>
   );
 };
